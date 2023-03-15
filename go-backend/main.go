65e54d870c9cfb22416e298c47c1bfd836c12bb9
Lloyd <dailoi280702@gmail.com>
2023-03-15 23:07:51 +0700

    create a simple grpc client with go and python

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/dailoi280702/se121/go_backend/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr    = flag.String("addr", "localhost:50051", "the address to connect to")
+	message = flag.String("name", "go", "Name to greet")
 )
 
 func main() {
+	// grpc
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := protos.NewHelloClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
+	defer cancel()
+
+	// router handler
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -18,6 +43,18 @@ func main() {
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("hello")); err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	r.Get("/say-hello", func(w http.ResponseWriter, r *http.Request) {
+		responce, err := c.SayHello(ctx, &protos.HelloRequest{Message: *message})
+		if err != nil {
+			log.Fatalf("could not say hello: %v", err)
+		}
+
+		msg := fmt.Sprintf("Greeting: %s", responce.GetMessage())
+		if _, err := w.Write([]byte(msg)); err != nil {
 			log.Fatal(err)
 		}
 	})
