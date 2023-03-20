a9d1cc10eb6ecbcb8d6d3778d218cfe4dfcef246
Lloyd <dailoi280702@gmail.com>
2023-03-21 01:22:49 +0700

    dockerize nextjs app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+const FetchHello = async () => {
+  // try {
+  const response = await fetch("http://localhost:8000/say-hello");
+  const data = await response.text();
+
+  if (!response.ok) return "???/";
+
+  return data;
+  // } catch {
+  //   return "error";
+  // }
+};
+
+export default async function Home() {
+  // const helloString = await FetchHello();
+
   return (
-    <div className="text-center bg-slate-800 h-screen text-gray-200">
-      New project, yayyy!
-    </div>
+    <>
+      <div className="text-center bg-slate-800 h-screen text-gray-200">
+        New project, yayyy!
+        {/* <div>{helloString}</div> */}
+      </div>
+    </>
   );
 }
