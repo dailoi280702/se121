e52a71739f66949241aca5724822c8d72f1d4cc9
Lloyd <dailoi280702@gmail.com>
2023-03-21 01:26:19 +0700

    fix: connecting frontend to api via docker network

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,24 @@
 const FetchHello = async () => {
-  // try {
-  const response = await fetch("http://localhost:8000/say-hello");
-  const data = await response.text();
+  try {
+    const response = await fetch("http://go-backend:8000/say-hello");
+    const data = await response.text();
 
-  if (!response.ok) return "???/";
+    if (!response.ok) return "???/";
 
-  return data;
-  // } catch {
-  //   return "error";
-  // }
+    return data;
+  } catch {
+    return "error";
+  }
 };
 
 export default async function Home() {
-  // const helloString = await FetchHello();
+  const helloString = await FetchHello();
 
   return (
     <>
       <div className="text-center bg-slate-800 h-screen text-gray-200">
         New project, yayyy!
-        {/* <div>{helloString}</div> */}
+        <div>{helloString}</div>
       </div>
     </>
   );
