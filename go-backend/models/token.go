3ed3f7b3ab2472371d481a5249d3a5035065197e
Lloyd <dailoi280702@gmail.com>
2023-04-02 14:52:34 +0700

    fix(web auth): public auth token fields

--- go-backend/models/token.go
+++ go-backend/models/token.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type AuthToken struct {
-	token     string
-	admin     bool
-	createdAt time.Time
-	expiresAt time.Time
+	Token     string
+	Admin     bool
+	CreatedAt time.Time
+	ExpiresAt time.Time
 }
 
 type TokenStore interface {
-	NewToken(lifetime time.Time) (string, error)
+	NewToken(lifetime time.Duration) (string, error)
 	IsExisting(token string) (bool, error)
 	IsExpired(token string) (bool, error)
 	Refesh(token string, lifetime time.Time) (string, error)
--- go-backend/store/memory/auth_token.go
+++ go-backend/store/memory/auth_token.go
@@ -4,16 +4,33 @@ import (
 	"time"
 
 	"github.com/dailoi280702/se121/go_backend/models"
+	"github.com/google/uuid"
 )
 
 // An implementation of TokenStore which its data are stored in machine memory
 type InMemoryTokenStore struct {
-	existingTokens []models.AuthToken
-	existedTokens  []models.AuthToken
+	existingTokens map[string]models.AuthToken
+	expiredTokens  map[string]models.AuthToken
 }
 
-func (s *InMemoryTokenStore) NewToken(lifetime time.Time) (string, error) {
-	return "", nil
+func NewInMemoryTokenStore() *InMemoryTokenStore {
+	return &InMemoryTokenStore{
+		existingTokens: map[string]models.AuthToken{},
+		expiredTokens:  map[string]models.AuthToken{},
+	}
+}
+
+func (s *InMemoryTokenStore) NewToken(lifetime time.Duration) (string, error) {
+	newToken := uuid.NewString()
+
+	s.existingTokens[newToken] = models.AuthToken{
+		token:     newToken,
+		admin:     false,
+		createdAt: time.Now(),
+		expiresAt: time.Now().Add(lifetime),
+	}
+
+	return newToken, nil
 }
 
 func (s *InMemoryTokenStore) IsExisting() (bool, error) {
