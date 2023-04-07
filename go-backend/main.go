d0b387a73861ff497e7e5f3baa133bc4c1f56318
Lloyd <dailoi280702@gmail.com>
2023-04-07 15:32:09 +0700

    feat(redis): test connection beetwen go api and redis image

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	api_v1 "github.com/dailoi280702/se121/go_backend/api/v1/router"
 	"github.com/dailoi280702/se121/go_backend/protos"
 	"github.com/go-chi/chi/v5"
+	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -22,6 +25,27 @@ func main() {
 	}
 	defer conn.Close()
 	c := protos.NewHelloClient(conn)
+
+	// redis
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// send SET command
+	err = client.Set(ctx, "foo", "bar", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	// send GET command and print the value
+	val, err := client.Get(ctx, "foo").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("foo", val)
 
 	// routes
 	router := chi.NewRouter()
