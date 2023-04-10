3768f14c276935a9eae9985ee41a1cd2f4fe710c
NBin17 <83058853+dkhoa1709@users.noreply.github.com>
2023-04-10 09:15:43 +0700

    fix(api auth): fix UI form Sign in

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -5,14 +5,12 @@ export default async function Page() {
   return (
     <>
       <div className="text-center">
-        <h2 className="text-xl font-bold text-neutral-800 p-14">
-          Sign in for more features
-        </h2>
+        <h2 className="text-xl font-bold text-neutral-800 p-14">SIGN IN</h2>
         <SignInForm />
         <div className="px-6 py-14 text-sm sm:py-6 sm:border-neutral-200 sm:bg-neutral-100 sm:border-0 sm:border-t">
           don&apos;t have an account?{' '}
-          <Link className="text-teal-600" href="/auth/signup">
-            Sign up
+          <Link className="text-teal-600" href="/auth/register">
+            Register
           </Link>{' '}
           for free
         </div>
--- frontend/components/forms/sign-in.tsx
+++ frontend/components/forms/sign-in.tsx
@@ -8,7 +8,16 @@ const Input = ({}: {}) => {
         className="w-full h-10 rounded-md indent-4 outline outline-1 text-base
         bg-transparent outline-neutral-200 placeholder-neutral-700
         focus:bg-neutral-100 focus:outline-2 focus:outline-teal-500"
-        placeholder="required*"
+        placeholder="Username or email"
+      />
+      <h3 className="text-xs bg-inherit focus-within:text-teal-50 font-medium">
+        Enter your password
+      </h3>
+      <input
+        className="w-full h-10 rounded-md indent-4 outline outline-1 text-base
+        bg-transparent outline-neutral-200 placeholder-neutral-700
+        focus:bg-neutral-100 focus:outline-2 focus:outline-teal-500"
+        placeholder="Password"
       />
       <h4 className="text-xs text-red-600">{null ? null : '\u2000'}</h4>
     </div>
@@ -18,7 +27,6 @@ const Input = ({}: {}) => {
 export default function SignInForm() {
   return (
     <form className="flex flex-col px-6 space-y-2 text-left text-sm sm:mb-14">
-      <Input />
       <Input />
       <button className="w-full h-10 rounded-md bg-teal-600 text-teal-50 !mt-8 font-medium">
         Sign in
