d30a95451ea90ff381bf00a15bec0e6765ef6c48
NBin17 <83058853+dkhoa1709@users.noreply.github.com>
2023-04-10 09:09:15 +0700

    fix(api auth): fix form register

---
+++ frontend/app/auth/register/page.tsx
@@ -0,0 +1,20 @@
+import RegisterForm from '@/components/forms/frm_register'
+import Link from 'next/link'
+
+export default async function Page() {
+  return (
+    <>
+      <div className="text-center">
+        <h2 className="text-xl font-bold text-neutral-800 p-14">REGISTER</h2>
+        <RegisterForm />
+        <div className="px-6 py-14 text-sm sm:py-6 sm:border-neutral-200 sm:bg-neutral-100 sm:border-0 sm:border-t">
+          Have an account?{' '}
+          <Link className="text-teal-600" href="/auth/signin">
+            Sign in
+          </Link>{' '}
+          to login website
+        </div>
+      </div>
+    </>
+  )
+}
