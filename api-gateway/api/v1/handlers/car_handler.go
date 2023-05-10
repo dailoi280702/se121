7bb895b52c5ef738799eecb1aa05b4beb7d78c94
Lloyd <dailoi280702@gmail.com>
2023-05-10 15:55:47 +0700

    feat(api gateway): init car handler

--- api-gateway/api/v1/handlers/car_handler.go
+++ api-gateway/api/v1/handlers/car_handler.go
@@ -1,7 +1,24 @@
 package handlers
 
-import "github.com/dailoi280702/se121/car-service/pkg/car"
+import (
+	"github.com/dailoi280702/se121/car-service/pkg/car"
+	"github.com/go-chi/chi/v5"
+)
 
 type CarHandler struct {
 	carService car.CarServiceClient
+}
+
+func NewCarHandler(carService car.CarServiceClient) *CarHandler {
+	return &CarHandler{
+		carService: carService,
+	}
+}
+
+func (h *CarHandler) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	// :TODO define routes
+
+	return r
 }
