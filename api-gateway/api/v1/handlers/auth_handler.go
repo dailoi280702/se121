b11e7ef48b08e6c80e50bbd1beb2b1d646438577
Lloyd <dailoi280702@gmail.com>
2023-05-12 18:46:35 +0700

    refactor (api gateway): remove unused code

--- api-gateway/api/v1/handlers/auth_handler.go
+++ api-gateway/api/v1/handlers/auth_handler.go
@@ -4,20 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	// "errors"
 	"flag"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/dailoi280702/se121/api-gateway/internal/service/auth"
-	// "github.com/dailoi280702/se121/api-gateway/internal/utils"
 	"github.com/dailoi280702/se121/api-gateway/models"
 	cached_store "github.com/dailoi280702/se121/api-gateway/store/cache"
 	"github.com/go-chi/chi/v5"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc/codes"
-	// "google.golang.org/grpc/status"
 )
 
 const (
@@ -31,7 +28,6 @@ type AuthHandler struct {
 	authService auth.AuthServiceClient
 }
 
-// :TODO serialize password
 func NewAuthHandler(redisClient *redis.Client, db *sql.DB, authService auth.AuthServiceClient) *AuthHandler {
 	return &AuthHandler{
 		tokenStore:  cached_store.NewRedisAuthTokenStore(redisClient),
@@ -100,53 +96,6 @@ func (h AuthHandler) signUp(w http.ResponseWriter, r *http.Request) {
 		convertWithPostFunc(func() {
 			log.Println(w.Write([]byte("ok")))
 		}))
-
-	// // get input
-	// input := signUpForm{}
-	// err := utils.DecodeJSONBody(w, r, &input)
-	// if err != nil {
-	// 	var mr *utils.MalformedRequest
-	// 	if errors.As(err, &mr) {
-	// 		http.Error(w, mr.Msg, mr.Status)
-	// 	} else {
-	// 		MustSendError(err, w)
-	// 	}
-	// 	return
-	// }
-	//
-	// // register user
-	// _, err = h.authService.SignUp(context.Background(), &auth.SignUpReq{
-	// 	Name:        input.Name,
-	// 	Email:       input.Email,
-	// 	Password:    input.Password,
-	// 	RePasssword: input.RePassword,
-	// })
-	//
-	// w.Header().Set("Content-Type", "application/json")
-	// if err != nil {
-	// 	code := status.Code(err)
-	// 	s, _ := status.FromError(err)
-	//
-	// 	switch code {
-	// 	case codes.InvalidArgument:
-	// 		w.WriteHeader(http.StatusBadRequest)
-	// 	case codes.AlreadyExists:
-	// 		w.WriteHeader(http.StatusConflict)
-	// 	case codes.Unavailable:
-	// 		w.WriteHeader(http.StatusServiceUnavailable)
-	// 		return
-	// 	default:
-	// 		MustSendError(err, w)
-	// 		return
-	// 	}
-	//
-	// 	_, err = w.Write([]byte(s.Message()))
-	// 	if err != nil {
-	// 		MustSendError(err, w)
-	// 	}
-	// 	return
-	// }
-	//
 }
 
 func (h AuthHandler) signOut(w http.ResponseWriter, r *http.Request) {
@@ -170,31 +119,9 @@ func (h AuthHandler) signOut(w http.ResponseWriter, r *http.Request) {
 				MustSendError(err, w)
 			}
 		}))
-	//
-	// _, err = h.authService.SignOut(context.Background(), &auth.SignOutReq{Token: c.Value})
-	// if err != nil {
-	// 	code := status.Code(err)
-	// 	switch code {
-	// 	case codes.Unavailable:
-	// 		http.Error(w, "auth service unavailable", http.StatusServiceUnavailable)
-	// 	default:
-	// 		MustSendError(err, w)
-	// 	}
-	// 	return
-	// }
-	//
-	// c.Value = ""
-	// c.MaxAge = -1
-	// c.Path = "/"
-	// http.SetCookie(w, c)
-	//
-	// if _, err := w.Write([]byte("signed out")); err != nil {
-	// 	MustSendError(err, w)
-	// }
 }
 
 func (h AuthHandler) refresh(w http.ResponseWriter, r *http.Request) {
-	// get token
 	c, err := r.Cookie(*cookieAuthToken)
 	if err != nil {
 		log.Println(err)
@@ -217,27 +144,4 @@ func (h AuthHandler) refresh(w http.ResponseWriter, r *http.Request) {
 				log.Panic(err)
 			}
 		}))
-
-	// res, err := h.authService.Refresh(context.Background(), &auth.RefreshReq{Token: c.Value})
-	// if err != nil {
-	// 	code := status.Code(err)
-	// 	switch code {
-	// 	case codes.NotFound:
-	// 		http.Error(w, "no user found", http.StatusNoContent)
-	// 	case codes.Unavailable:
-	// 		http.Error(w, "service unabailable", http.StatusServiceUnavailable)
-	// 	default:
-	// 		MustSendError(err, w)
-	// 	}
-	// 	return
-	// }
-	//
-	// c.Value = res.GetToken()
-	// c.Path = "/"
-	// http.SetCookie(w, c)
-	//
-	// w.Header().Set("Content-Type", "application/json")
-	// if err := json.NewEncoder(w).Encode(res.GetUser()); err != nil {
-	// 	log.Panic(err)
-	// }
 }
