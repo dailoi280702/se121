e42aacc8764184ea555182960c3864830881062d
Lloyd <dailoi280702@gmail.com>
2023-05-14 23:19:59 +0700

    refactor(car service): refactor create car handler using go routine

--- car-service/internal/server/car.go
+++ car-service/internal/server/car.go
@@ -72,67 +72,66 @@ func (s *carSerivceServer) CreateCar(ctx context.Context, req *car.CreateCarReq)
 		})
 	}
 
-	// Verify brand and series existence
-	if req.BrandId != nil {
-		id := req.GetBrandId()
-		exists, err := dbIdExists(s.db, "car_brands", id)
-		if err != nil {
-			return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
+	errCh := make(chan error, 1)
+	go func() {
+		// Verify brand and series existence
+		if req.BrandId != nil {
+			id := req.GetBrandId()
+			exists, err := dbIdExists(s.db, "car_brands", id)
+			errCh <- err
+			if !exists {
+				validationErrors["brandId"] = fmt.Sprintf("Brand %d does not exist", id)
+			}
 		}
-		if !exists {
-			validationErrors["brandId"] = fmt.Sprintf("Brand %d does not exist", id)
-		}
-	}
-	if req.SeriesId != nil {
-		id := req.GetSeriesId()
-		exists, err := dbIdExists(s.db, "car_series", id)
-		if err != nil {
-			return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
-		}
-		if !exists {
-			validationErrors["seriesId"] = fmt.Sprintf("Series %d does not exist", id)
-		} else {
-			_, ok := validationErrors["brandId"]
-			if !ok {
-				if req.BrandId != nil {
-					brandId := req.GetBrandId()
-					match, err := dbSeriesBrandMatches(s.db, id, brandId)
-					if err != nil {
-						return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
+		if req.SeriesId != nil {
+			id := req.GetSeriesId()
+			exists, err := dbIdExists(s.db, "car_series", id)
+			errCh <- err
+			if !exists {
+				validationErrors["seriesId"] = fmt.Sprintf("Series %d does not exist", id)
+			} else {
+				_, ok := validationErrors["brandId"]
+				if !ok {
+					if req.BrandId != nil {
+						brandId := req.GetBrandId()
+						match, err := dbSeriesBrandMatches(s.db, id, brandId)
+						errCh <- err
+						if !match {
+							validationErrors["seriesId"] = fmt.Sprintf("Series %d does not exist in brand %d", id, brandId)
+						}
+					} else {
+						brandId, err := dbGetBrandIdBySeriesId(s.db, int(id))
+						errCh <- err
+						brandId32 := int32(brandId)
+						req.BrandId = &brandId32
 					}
-					if !match {
-						validationErrors["seriesId"] = fmt.Sprintf("Series %d does not exist in brand %d", id, brandId)
-					}
-				} else {
-					brandId, err := dbGetBrandIdBySeriesId(s.db, int(id))
-					if err != nil {
-						return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
-					}
-					brandId32 := int32(brandId)
-					req.BrandId = &brandId32
+				}
+			}
+
+			if req.FuelTypeId != nil {
+				id := req.GetFuelTypeId()
+				exists, err := dbIdExists(s.db, "fuel_types", id)
+				errCh <- err
+				if !exists {
+					validationErrors["fuel"] = fmt.Sprintf("Fuel type %d does not exist", id)
+				}
+			}
+			if req.TransmissionId != nil {
+				id := req.GetTransmissionId()
+				exists, err := dbIdExists(s.db, "car_transmissions", id)
+				errCh <- err
+				if !exists {
+					validationErrors["transmission"] = fmt.Sprintf("Transmission %d does not exist", id)
 				}
 			}
 		}
+		close(errCh)
+	}()
 
-		if req.FuelTypeId != nil {
-			id := req.GetFuelTypeId()
-			exists, err := dbIdExists(s.db, "fuel_types", id)
-			if err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
-			}
-			if !exists {
-				validationErrors["fuel"] = fmt.Sprintf("Fuel type %d does not exist", id)
-			}
-		}
-		if req.TransmissionId != nil {
-			id := req.GetTransmissionId()
-			exists, err := dbIdExists(s.db, "car_transmissions", id)
-			if err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
-			}
-			if !exists {
-				validationErrors["transmission"] = fmt.Sprintf("Transmission %d does not exist", id)
-			}
+	for range errCh {
+		err := <-errCh
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "car service error: %v", err)
 		}
 	}
 
@@ -145,11 +144,11 @@ func (s *carSerivceServer) CreateCar(ctx context.Context, req *car.CreateCarReq)
 
 	// Insert car into the database
 	_, err := s.db.Exec(`
-        insert into car_models (brand_id, series_id, name, year, horsepower, torque, transmission, fuel_type, review, image_url)
-        values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 )
-        `, req.BrandId, req.SeriesId, req.Name, req.Year, req.HorsePower, req.Torque, req.TransmissionId, req.FuelTypeId, req.Review, req.ImageUrl)
+    insert into car_models (brand_id, series_id, name, year, horsepower, torque, transmission, fuel_type, review, image_url)
+    values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 )
+    `, req.BrandId, req.SeriesId, req.Name, req.Year, req.HorsePower, req.Torque, req.TransmissionId, req.FuelTypeId, req.Review, req.ImageUrl)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "error while inserting car %v to db: %v", req, err)
+		return nil, status.Errorf(codes.Internal, "car server got error while inserting car %v to db: %v", req, err)
 	}
 
 	return &car.Empty{}, nil
