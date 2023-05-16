fc76dd7ccfa913df96ffc8605789d53f850a6134
Lloyd <dailoi280702@gmail.com>
2023-05-16 15:42:59 +0700

    feat(car service): implement search for brands

--- car-service/internal/server/brand.go
+++ car-service/internal/server/brand.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"sync"
 
 	"github.com/dailoi280702/se121/car-service/pkg/car"
 	"google.golang.org/grpc/codes"
@@ -28,6 +31,107 @@ func (s *carSerivceServer) UpdateBrand(context.Context, *car.UpdateBrandReq) (*c
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateBrand not implemented")
 }
 
-func (s *carSerivceServer) SearchForBrand(context.Context, *car.SearchReq) (*car.SearchForBrandRes, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SearchForCar not implemented")
+func (s *carSerivceServer) SearchForBrand(ctx context.Context, req *car.SearchReq) (*car.SearchForBrandRes, error) {
+	res := car.SearchForBrandRes{}
+	errCh := make(chan error)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		query := generateSearchForBrandsQuery(req)
+		brands, err := fetchBrands(s.db, query)
+		errCh <- err
+		res.Brands = brands
+		defer wg.Done()
+	}()
+
+	go func() {
+		total, err := dbCountRecords(s.db, "car_brands")
+		errCh <- err
+		res.Total = int32(total)
+		defer wg.Done()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	for err := range errCh {
+		if err != nil {
+			return nil, serverError(err)
+		}
+	}
+
+	return &res, nil
+}
+
+func generateSearchForBrandsQuery(req *car.SearchReq) string {
+	query := `
+    SELECT id, name, country_of_origin, founded_year ,website_url, logo_url
+    FROM car_brands
+    WHERE 1=1`
+
+	// Add search conditions if a query is provided
+	if req.GetQuery() != "" {
+		query += fmt.Sprintf(` 
+            AND (name ILIKE '%%%s%%'
+            OR country_of_origin ILIKE '%%%s%%')`,
+			req.GetQuery(), req.GetQuery())
+	}
+
+	// Add ordering if orderby field is provided
+	if req.GetOrderby() != "" {
+		orderBy := "car_brands.created_at"
+		switch req.GetOrderby() {
+		case "date":
+			orderBy = "car_brands.created_at"
+		case "country":
+			orderBy = "car_brands.country_of_origin"
+		case "year":
+			orderBy = "car_brands.founded_year"
+		case "name":
+			orderBy = "car_brands.name"
+		}
+		query += fmt.Sprintf(" ORDER BY %s", orderBy)
+		if req.GetIsAscending() {
+			query += " ASC"
+		} else {
+			query += " DESC"
+		}
+	}
+
+	// Add pagination if startAt field is provided
+	if req.GetStartAt() > 0 {
+		query += fmt.Sprintf(" OFFSET %d", req.GetStartAt())
+	}
+
+	// Add limit if limit field is provided
+	if req.GetLimit() > 0 {
+		query += fmt.Sprintf(" LIMIT %d", req.GetLimit())
+	}
+
+	return query
+}
+
+func fetchBrands(db *sql.DB, query string) ([]*car.Brand, error) {
+	brands := []*car.Brand{}
+	rows, err := db.Query(query)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return brands, nil
+		}
+		return nil, fmt.Errorf("failed to get records: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var brand car.Brand
+		err := rows.Scan(&brand.Id, &brand.Name, &brand.CountryOfOrigin, &brand.FoundedYear, &brand.LogoUrl, &brand.WebsiteUrl)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get record: %v", err)
+		}
+		brands = append(brands, &brand)
+	}
+	return brands, nil
 }
