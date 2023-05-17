fb3b6536a33a02334f4bc9048135a1237eeaee20
Lloyd <dailoi280702@gmail.com>
2023-05-17 17:32:44 +0700

    feat(api gateway): implement series routes handlers

--- api-gateway/api/v1/handlers/series_handler.go
+++ api-gateway/api/v1/handlers/series_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/dailoi280702/se121/car-service/pkg/car"
@@ -20,20 +21,58 @@ func NewSeriesRoutes(carService car.CarServiceClient) chi.Router {
 
 func handleGetSeries(carService car.CarServiceClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req car.GetSeriesReq
+		var res *car.Series
+		convertJsonApiToGrpc(w, r,
+			func() error {
+				var err error
+				res, err = carService.GetSeries(context.Background(), &req)
+				return err
+			},
+			convertWithJsonReqData(&req),
+			convertWithPostFunc(func() {
+				SendJson(w, res)
+			}))
 	}
 }
 
 func handleUpdateSeries(carService car.CarServiceClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req car.UpdateSeriesReq
+		convertJsonApiToGrpc(w, r,
+			func() error {
+				_, err := carService.UpdateSeries(context.Background(), &req)
+				return err
+			},
+			convertWithJsonReqData(&req))
 	}
 }
 
 func handleCreateSeries(carService car.CarServiceClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req car.CreateSeriesReq
+		convertJsonApiToGrpc(w, r,
+			func() error {
+				_, err := carService.CreateSeries(context.Background(), &req)
+				return err
+			},
+			convertWithJsonReqData(&req))
 	}
 }
 
 func handleSearchSeries(carService car.CarServiceClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req car.SearchReq
+		var res *car.SearchForSeriesRes
+		convertJsonApiToGrpc(w, r,
+			func() error {
+				var err error
+				res, err = carService.SearchForSeries(context.Background(), &req)
+				return err
+			},
+			convertWithJsonReqData(&req),
+			convertWithPostFunc(func() {
+				SendJson(w, res)
+			}))
 	}
 }
