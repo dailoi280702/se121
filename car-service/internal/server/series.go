1d9b5c7f6851fb342cc71ac59c4af0b9ba1e8232
Lloyd <dailoi280702@gmail.com>
2023-05-17 17:13:35 +0700

    feat(car service): implement function to validate and vefiry series

--- car-service/internal/server/series.go
+++ car-service/internal/server/series.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/dailoi280702/se121/car-service/pkg/car"
 	"google.golang.org/grpc/codes"
@@ -22,8 +24,11 @@ func (s *carSerivceServer) GetSeries(ctx context.Context, req *car.GetSeriesReq)
 	return nil, status.Errorf(codes.Unimplemented, "method GetSeries not implemented")
 }
 
-func (s *carSerivceServer) CreateSeries(context.Context, *car.CreateSeriesReq) (*car.Empty, error) {
+func (s *carSerivceServer) CreateSeries(ctx context.Context, req *car.CreateSeriesReq) (*car.Empty, error) {
 	// Validate and verify inputs
+	if err := validateSeries(s.db, &req.Name, &req.BrandId); err != nil {
+		return nil, err
+	}
 
 	// Insert series into database
 
@@ -38,6 +43,9 @@ func (s *carSerivceServer) UpdateSeries(ctx context.Context, req *car.UpdateSeri
 	}
 
 	// Validate and verify inputs
+	if err := validateSeries(s.db, &req.Name, &req.BrandId); err != nil {
+		return nil, err
+	}
 
 	// Prepare update data
 
@@ -50,7 +58,7 @@ func (s *carSerivceServer) SearchForSeries(context.Context, *car.SearchReq) (*ca
 	return nil, status.Errorf(codes.Unimplemented, "method SearchForCar not implemented")
 }
 
-// check for series existence in database
+// check for series existence in database by its id
 // if series does not exist return error
 func checkSeriesExistence(db *sql.DB, id int32) error {
 	exists, err := dbIdExists(db, "car_series", id)
@@ -66,6 +74,64 @@ func checkSeriesExistence(db *sql.DB, id int32) error {
 // validate and verify series inputs before modify database
 // return an json encoded errorResponse as error if inputs are not in correct formats
 func validateSeries(db *sql.DB, name *string, brandID *int32) error {
+	validateErrors := map[string]string{}
+
+	// validate name and brandID
+	if name != nil {
+		if strings.TrimSpace(*name) == "" {
+			validateErrors["name"] = "Name can not be empty"
+		}
+	}
+	if brandID != nil {
+		if *brandID == 0 {
+			validateErrors["brandId"] = "Brand can not be empty"
+		}
+	}
+
+	// return an error if validation failed
+	if len(validateErrors) > 0 {
+		return convertGrpcToJsonError(codes.InvalidArgument, errorResponse{Details: validateErrors})
+	}
+
+	errCh := make(chan error)
+	var wg sync.WaitGroup
+	go func() {
+		wg.Done()
+		close(errCh)
+	}()
+
+	// verify name
+	go func() {
+		nameExists, err := dbExists(db, `
+            SELECT EXISTS(SELECT 1 FROM car_series WHERE name = $1
+            `, *name)
+		errCh <- err
+		if nameExists {
+			validateErrors["name"] = fmt.Sprintf("Series %s already exists", *name)
+		}
+	}()
+
+	// verify brand
+	go func() {
+		brandExists, err := dbIdExists(db, "car_brands", *brandID)
+		errCh <- err
+		if !brandExists {
+			validateErrors["brandId"] = fmt.Sprintf("Brand %d does not exist", *brandID)
+		}
+	}()
+
+	// return an error if verification failed
+	if len(validateErrors) > 0 {
+		return convertGrpcToJsonError(codes.InvalidArgument, errorResponse{Details: validateErrors})
+	}
+
+	// return interal error if exist
+	for err := range errCh {
+		if err != nil {
+			return serverError(err)
+		}
+	}
+
 	return nil
 }
 
