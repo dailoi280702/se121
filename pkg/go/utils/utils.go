0443d85d24fa08dbbb4ba5327242998e6e18751e
Lloyd <dailoi280702@gmail.com>
2023-05-19 08:51:56 +0700

    chore: create shared pakages for go

---
+++ pkg/go/utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func ConvertGrpcToJsonError(c codes.Code, e any) error {
+	if e == nil {
+		return nil
+	}
+
+	data, err := json.Marshal(e)
+	if err != nil {
+		return status.Error(codes.Internal, fmt.Sprintf("car service err: %v", err))
+	}
+	return status.Error(c, string(data))
+}
