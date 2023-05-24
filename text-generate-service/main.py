c706cdfdd270a97ab00db6612dca01afaeeb108f
Lloyd <dailoi280702@gmail.com>
2023-05-25 00:15:38 +0700

    feat(text generate service): serving text generate service

--- text-generate-service/main.py
+++ text-generate-service/main.py
@@ -3,22 +3,47 @@ import logging
 import asyncio
 from concurrent import futures
 
-from protos import hello_pb2_grpc
-from protos import hello_pb2
+from protos import text_generate_pb2
+from protos import text_generate_pb2_grpc
 
 
-class HelloServicer(hello_pb2_grpc.HelloServicer):
-    async def SayHello(
-        self, request: hello_pb2.HelloRequest, context: grpc.aio.ServicerContext
-    ) -> hello_pb2.HelloResponse:
-        return hello_pb2.HelloResponse(
-            message="hello %s from python!" % request.message
-        )
+# class HelloServicer(hello_pb2_grpc.HelloServicer):
+#     async def SayHello(
+#         self, request: hello_pb2.HelloRequest, context: grpc.aio.ServicerContext
+#     ) -> hello_pb2.HelloResponse:
+#         return hello_pb2.HelloResponse(
+#             message="hello %s from python!" % request.message
+#         )
+
+
+class TextGenerateServicer:
+    async def GenerateCarReview(
+        self,
+        request: text_generate_pb2.GenerateReviewReq,
+        context: grpc.aio.ServicerContext,
+    ) -> text_generate_pb2.ResString:
+        return text_generate_pb2.ResString(text="fugg")
+        # context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        # context.set_details("Method not implemented!")
+        # raise NotImplementedError("Method not implemented!")
+
+    async def GenerateBlogSummarization(
+        self,
+        request: text_generate_pb2.GenerateBlogSummarizationReq,
+        context: grpc.aio.ServicerContext,
+    ) -> text_generate_pb2.ResString:
+        return text_generate_pb2.ResString(text="fugg")
+        # context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        # context.set_details("Method not implemented!")
+        # raise NotImplementedError("Method not implemented!")
 
 
 async def serveGrpc() -> None:
     server = grpc.aio.server()
-    hello_pb2_grpc.add_HelloServicer_to_server(HelloServicer(), server)
+    # hello_pb2_grpc.add_HelloServicer_to_server(HelloServicer(), server)
+    text_generate_pb2_grpc.add_TextGenerateServiceServicer_to_server(
+        TextGenerateServicer(), server
+    )
     listen_addr = "[::]:50051"
     server.add_insecure_port(listen_addr)
     logging.info("Starting server on %s", listen_addr)
