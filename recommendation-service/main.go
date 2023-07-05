ae22b6859655764de00bb86dc58542d2eab9e2ef
Lloyd <dailoi280702@gmail.com>
2023-07-05 12:16:57 +0700

    feat(recommendation service): implement get user recommended blgos

--- recommendation-service/main.go
+++ recommendation-service/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/dailoi280702/se121/blog-service/pkg/blog"
 	"github.com/dailoi280702/se121/recommendation-service/internal/server"
 	"github.com/dailoi280702/se121/recommendation-service/pkg/recommendation"
+	"github.com/dailoi280702/se121/user-service/userpb"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -19,7 +20,17 @@ import (
 var (
 	serverAddress   = flag.String("server address", "[::]:50051", "address of recommendation server")
 	blogServicePort = flag.String("blogServicePort", "blog-service:50051", "the address to connect to blog service")
+	userServicePort = flag.String("userServicePort", "user-service:50051", "the address to connect to user service")
 )
+
+func NewUserService(ctx context.Context) (*grpc.ClientConn, user.UserServiceClient) {
+	conn, err := grpc.DialContext(ctx, *userServicePort, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect user service: %v", err)
+	}
+
+	return conn, user.NewUserServiceClient(conn)
+}
 
 func NewBlogService(ctx context.Context) (*grpc.ClientConn, blog.BlogServiceClient) {
 	conn, err := grpc.DialContext(ctx, *blogServicePort, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -30,14 +41,14 @@ func NewBlogService(ctx context.Context) (*grpc.ClientConn, blog.BlogServiceClie
 	return conn, blog.NewBlogServiceClient(conn)
 }
 
-func serveServer(db *sql.DB, blogService blog.BlogServiceClient) {
+func serveServer(db *sql.DB, blogService blog.BlogServiceClient, userService user.UserServiceClient) {
 	lis, err := net.Listen("tcp", *serverAddress)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	sv := grpc.NewServer()
-	recommendation.RegisterRecommendationServiceServer(sv, server.NewServer(db, blogService))
+	recommendation.RegisterRecommendationServiceServer(sv, server.NewServer(db, blogService, userService))
 	if err := sv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve server: %v", err)
 	}
@@ -49,11 +60,14 @@ func main() {
 		log.Fatalf("failed to connect to db :%v", err)
 	}
 
-	blogServiceConn, blogService := NewBlogService(context.Background())
+	ctx := context.Background()
+	blogServiceConn, blogService := NewBlogService(ctx)
+	userServiceConn, userService := NewUserService(ctx)
 	defer func() {
 		db.Close()
+		userServiceConn.Close()
 		blogServiceConn.Close()
 	}()
 
-	serveServer(db, blogService)
+	serveServer(db, blogService, userService)
 }
